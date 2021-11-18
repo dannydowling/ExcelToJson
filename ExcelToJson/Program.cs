2a21299cfb2a092614e05b89511380bc6c7854bc
Danny Dowling <danny.dowling@gmail.com>
2021-11-18 14:35:31 -0900

    using File.Exists

--- ExcelToJson/Program.cs
+++ ExcelToJson/Program.cs
@@ -14,15 +14,17 @@ namespace ExcelToJson
 
             var inFilePath = "worldairports.xlsx";
 
-
+            //create a list of countries and the locations in them in a sorted set
             SortedList<string, Location> list = new SortedList<string, Location>();
 
+            //open the file for reading
             using (var inFile = File.Open(inFilePath, FileMode.Open, FileAccess.Read))
 
             {
                 using (var reader = ExcelReaderFactory.CreateReader(inFile, new ExcelReaderConfiguration()
                 { FallbackEncoding = Encoding.GetEncoding(1252) }))
 
+                    //try to read the whole document and make classes from it
                     while (reader.Read())
                     {
                         try
@@ -43,14 +45,47 @@ namespace ExcelToJson
                         }
                         catch (Exception)
                         {
-
+                            //if we can't read the record, skip it.
                             reader.Read();
                         }
 
 
+                        //create a list of the countries in a new file
+                        foreach (var countryName in list.Keys.Distinct())
+                        {
+                            if (!File.Exists(@".\Positions\" + countryName))
+                            {
+                                File.Create(@".\Positions\" + countryName);
+                            }                            
+                            using (var countryFile = (File.CreateText(@".\Positions\Countries.txt")))
+                            {
+                                using (var writer = new JsonTextWriter(countryFile))
+                                {
+                                    try
+                                    {
+                                        writer.WriteStartObject();
+                                        writer.WritePropertyName("country");
+                                        writer.WriteValue(reader.GetString(6).ToString());
+                                    }
+                                    catch (Exception)
+                                    {
+                                        reader.NextResult();
+                                    }
+
+                                }
+                            };
+                        }
+
+                        //create a list of locations within country files
                         foreach (var Country in list.Keys.Distinct())
                         {
                             var countryFileString = String.Format(@".\Positions\{0}.txt", Country);
+                            if (!File.Exists(@".\Positions\" + countryFileString))
+                            {
+                                File.Create(@".\Positions\" + countryFileString);
+                            }
+                            
+                            File.CreateText(@".\Positions\" + countryFileString);
                             using (var countryFile = (File.CreateText(countryFileString)))
                             {
                                 using (var writer = new JsonTextWriter(countryFile))
@@ -96,28 +131,6 @@ namespace ExcelToJson
                                     } while (reader.NextResult());
                                     writer.WriteEndArray();
                                 }
-                            }
-
-
-                            foreach (var countryName in list.Keys.Distinct())
-                            {
-                                using (var countryFile = (File.CreateText(@".\Positions\Countries.txt")))
-                                {
-                                    using (var writer = new JsonTextWriter(countryFile))
-                                    {
-                                        try
-                                        {
-                                            writer.WriteStartObject();
-                                            writer.WritePropertyName("country");
-                                            writer.WriteValue(reader.GetString(6).ToString());
-                                        }
-                                        catch (Exception)
-                                        {
-                                            reader.NextResult();
-                                        }
-
-                                    }
-                                };
                             }
                         }
                     }
