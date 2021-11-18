9bdbea90ac3ca93009a42254d90527879fb85d40
Danny Dowling <danny.dowling@gmail.com>
2021-11-18 14:23:57 -0900

    trying out logic in the excel to json parser

---
+++ ExcelToJson/Location.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelToJson
+{
+    internal class Location
+    {
+        public string name { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string country { get; set; }
+        public string icao { get; set; }
+
+        public double lat { get; set; }
+        public double lon { get; set; }
+    }
+}
--- ExcelToJson/Program.cs
+++ ExcelToJson/Program.cs
@@ -9,60 +9,118 @@ namespace ExcelToJson
     class Program
     {
         static void Main()
-        {            
+        {
             Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
 
             var inFilePath = "worldairports.xlsx";
-            var outFilePath = "Positions.txt";
+
+
+            SortedList<string, Location> list = new SortedList<string, Location>();
 
             using (var inFile = File.Open(inFilePath, FileMode.Open, FileAccess.Read))
-            using (var outFile = File.CreateText(outFilePath))
+
             {
                 using (var reader = ExcelReaderFactory.CreateReader(inFile, new ExcelReaderConfiguration()
                 { FallbackEncoding = Encoding.GetEncoding(1252) }))
-                using (var writer = new JsonTextWriter(outFile))
-                {
-                    writer.Formatting = Formatting.Indented;
-                    writer.WriteStartArray();
-                    reader.Read(); //skip the headings
-                    do
+
+                    while (reader.Read())
                     {
-                        while (reader.Read())
+                        try
                         {
-                            try
+                            Location location = new Location()
                             {
-                                writer.WriteStartObject();
-                                writer.WritePropertyName("state");
-                                writer.WriteValue(reader.GetString(0).ToString());
+                                country = reader.GetString(6).ToString(),
+                                state = reader.GetString(0).ToString(),
+                                city = reader.GetString(1).ToString(),
+                                name = reader.GetString(2).ToString(),
+                                icao = reader.GetString(3).ToString(),
+                                lat = reader.GetDouble(4),
+                                lon = reader.GetDouble(5)
+                            };
 
-                                writer.WritePropertyName("city");
-                                writer.WriteValue(reader.GetString(1).ToString());
+                            list.Add(reader.GetString(6).ToString(), location);
 
-                                writer.WritePropertyName("name");
-                                writer.WriteValue(reader.GetString(2).ToString());
-
-                                writer.WritePropertyName("icao");
-                                writer.WriteValue(reader.GetString(3).ToString());
-
-                                writer.WritePropertyName("lat");
-                                writer.WriteValue(reader.GetDouble(4));
-
-                                writer.WritePropertyName("lon");
-                                writer.WriteValue(reader.GetDouble(5));
-
-                                writer.WritePropertyName("country");
-                                writer.WriteValue(reader.GetString(6).ToString());
-
-                                writer.WriteEndObject();
-                            }
-                            catch (Exception)
-                            {
-                                reader.NextResult();
-                            }                          
                         }
-                    } while (reader.NextResult());
-                    writer.WriteEndArray();
-                }
+                        catch (Exception)
+                        {
+
+                            reader.Read();
+                        }
+
+
+                        foreach (var Country in list.Keys.Distinct())
+                        {
+                            var countryFileString = String.Format(@".\Positions\{0}.txt", Country);
+                            using (var countryFile = (File.CreateText(countryFileString)))
+                            {
+                                using (var writer = new JsonTextWriter(countryFile))
+                                {
+                                    writer.Formatting = Formatting.Indented;
+                                    writer.WriteStartArray();
+                                    reader.Read(); //skip the headings
+                                    do
+                                    {
+                                        while (reader.Read())
+                                        {
+                                            try
+                                            {
+                                                writer.WriteStartObject();
+                                                writer.WritePropertyName("country");
+                                                writer.WriteValue(reader.GetString(6).ToString());
+
+                                                writer.WritePropertyName("state");
+                                                writer.WriteValue(reader.GetString(0).ToString());
+
+                                                writer.WritePropertyName("city");
+                                                writer.WriteValue(reader.GetString(1).ToString());
+
+                                                writer.WritePropertyName("name");
+                                                writer.WriteValue(reader.GetString(2).ToString());
+
+                                                writer.WritePropertyName("icao");
+                                                writer.WriteValue(reader.GetString(3).ToString());
+
+                                                writer.WritePropertyName("lat");
+                                                writer.WriteValue(reader.GetDouble(4));
+
+                                                writer.WritePropertyName("lon");
+                                                writer.WriteValue(reader.GetDouble(5));
+
+                                                writer.WriteEndObject();
+                                            }
+                                            catch (Exception)
+                                            {
+                                                reader.NextResult();
+                                            }
+                                        }
+                                    } while (reader.NextResult());
+                                    writer.WriteEndArray();
+                                }
+                            }
+
+
+                            foreach (var countryName in list.Keys.Distinct())
+                            {
+                                using (var countryFile = (File.CreateText(@".\Positions\Countries.txt")))
+                                {
+                                    using (var writer = new JsonTextWriter(countryFile))
+                                    {
+                                        try
+                                        {
+                                            writer.WriteStartObject();
+                                            writer.WritePropertyName("country");
+                                            writer.WriteValue(reader.GetString(6).ToString());
+                                        }
+                                        catch (Exception)
+                                        {
+                                            reader.NextResult();
+                                        }
+
+                                    }
+                                };
+                            }
+                        }
+                    }
             }
         }
     }
